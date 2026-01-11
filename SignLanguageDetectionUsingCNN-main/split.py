266b8d8761eba1d69168ea7857a10d2e3e599c92
MARIEM KBAIER <mariem.kbaier@enis.tn>
2026-01-11 16:41:29 +0100

    second commit of SignLanguageDetectionUsingCNN project

--- SignLanguageDetectionUsingCNN-main/split.py
+++ SignLanguageDetectionUsingCNN-main/split.py
@@ -1,3 +1,3 @@
 import splitfolders
-dr = 'SignImage48x48'
-splitfolders.ratio(dr,"splitdataset48x48" ,ratio=(0.8,0.2))
+dr = 'asl_dataset'
+splitfolders.ratio(dr,"Splitasl_dataset" ,ratio=(0.8,0.2))
